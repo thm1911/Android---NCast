462449e387030fa5da5241e517aef2bad80b69b3
PT <thambocap191104@gmail.com>
2024-10-17 17:21:17 +0700

    add: SignIn/SignUp

---
+++ app/src/main/java/com/example/ncast/database/user/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.example.ncast.database.user
+
+import android.app.Application
+import com.example.ncast.database.NCastDatabase
+import com.example.ncast.model.User
+
+class UserRepository(application: Application) {
+    private val userDAO: UserDAO
+    init {
+        userDAO = NCastDatabase.getInstance(application).userDAO()
+    }
+
+    suspend fun insertUser(user: User): Long = userDAO.insertUser(user)
+    suspend fun isEmailExist(email: String): Boolean = userDAO.isEmailExist(email)
+    suspend fun isUsernameExist(username: String): Boolean = userDAO.isUsernameExist(username)
+    suspend fun checkUserSignIn(username: String,password: String): Boolean = userDAO.checkUserSignIn(username, password)
+    suspend fun getUserIdFromUsername(username: String): Long = userDAO.getUserIdFromUsername(username)
+}
