b1acb86164238f006547aeab75ba0848feb938a5
jinjja9 <hth2012004@gmail.com>
2024-10-20 20:36:37 +0700

    constraint layout

--- app/src/main/java/com/example/ncast/ui/mainApp/home/HomeFragment.kt
+++ app/src/main/java/com/example/ncast/ui/mainApp/home/HomeFragment.kt
@@ -1,4 +1,5 @@
 package com.example.ncast.ui.mainApp.home
+
 import com.example.ncast.adapter.recyclerViewHomeAdapter.ContinueCategoryAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,41 +8,53 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.ncast.R
 import com.example.ncast.SpacingItem
 import com.example.ncast.adapter.recyclerViewHomeAdapter.RecentMusicListeningAdapter
 import com.example.ncast.adapter.recyclerViewHomeAdapter.TopTrendingAdapter
+import com.example.ncast.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         // Continue Listening
-        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView_continue)
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)// cột
+        binding.recyclerViewContinue.layoutManager = GridLayoutManager(requireContext(), 2) // cột
         val adapter = ContinueCategoryAdapter()
-        recyclerView.adapter = adapter
+        binding.recyclerViewContinue.adapter = adapter
 
-        //Top Trending
-        val recyclerViewTrending: RecyclerView = view.findViewById(R.id.recyclerView_top_trending)
-        recyclerViewTrending.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        // Top Trending
+        binding.recyclerViewTopTrending.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         val adapterTrending = TopTrendingAdapter()
-        recyclerViewTrending.adapter = adapterTrending
+        binding.recyclerViewTopTrending.adapter = adapterTrending
 
         // Recent Music Listening
-        val recyclerViewRecentMusic: RecyclerView = view.findViewById(R.id.recyclerView_recent_music_listening)
-        recyclerViewRecentMusic.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerViewRecentMusicListening.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         val adapterRecentMusic = RecentMusicListeningAdapter()
-        recyclerViewRecentMusic.adapter = adapterRecentMusic
-        val space = resources.getDimensionPixelSize(R.dimen.space)
-        recyclerView.addItemDecoration(SpacingItem(space))
+        binding.recyclerViewRecentMusicListening.adapter = adapterRecentMusic
 
-        return view
+        // Adding spacing
+        val space = resources.getDimensionPixelSize(R.dimen.space)
+        binding.recyclerViewContinue.addItemDecoration(SpacingItem(space))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
